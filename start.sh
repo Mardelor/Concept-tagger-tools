4be1042d4c40e6a704b7c18286570bd2cbad0ea1
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-04-02 11:19:25 +0200

    Add git config in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~
 mkdir Projects && cd Projects
+git config --global user.name "Mardelor"
+git config --global user.email remy.zirnheld@telecom-sudparis.eu
 git clone https://github.com/Mardelor/Concept-Tagger.git
