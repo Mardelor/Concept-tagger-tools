48a44ea4a108deafddc33a357425ff6dc772cfca
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-04-30 14:24:53 +0200

    Hot fix : update git config

--- start.sh
+++ start.sh
@@ -5,7 +5,4 @@ git config --global user.name "Mardelor"
 git config --global user.email remy.zirnheld@telecom-sudparis.eu
 git clone https://github.com/Mardelor/Concept-Tagger.git
 
-git config --global user.name "Rémy Zirnheld"
-git config --global user.email remy.zirnheld@insee.fr
-
 cat Concept-Tagger/bashrc.txt >> ~/.bashrc
