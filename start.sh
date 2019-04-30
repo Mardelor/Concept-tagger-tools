cb0403f4f25542e9db630a14111b7948246d016c
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-04-30 12:17:57 +0000

    Change git config user name

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 mkdir Projects && cd Projects
-git config --global user.name "Mardelor"
+git config --global user.name Mardelor
 git config --global user.email remy.zirnheld@telecom-sudparis.eu
 git clone https://github.com/Mardelor/Concept-Tagger.git
 
