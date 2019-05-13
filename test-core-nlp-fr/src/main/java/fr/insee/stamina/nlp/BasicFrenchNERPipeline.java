c414629aa33540e6fa4817c490693e22ce3193d8
Rémy Zirnheld <remy.zirnheld@insee.fr>
2019-05-13 12:26:42 +0000

    First try with tokensregex ! Seems to work :)

--- test-core-nlp-fr/src/main/java/fr/insee/stamina/nlp/BasicFrenchNERPipeline.java
+++ test-core-nlp-fr/src/main/java/fr/insee/stamina/nlp/BasicFrenchNERPipeline.java
@@ -1,17 +1,22 @@
 package fr.insee.stamina.nlp;
 
 import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class BasicFrenchNERPipeline {
+
     public static void main(String[] args) throws IOException {
 
         Properties properties = new Properties();
@@ -19,7 +24,7 @@ public class BasicFrenchNERPipeline {
         System.out.println("Default pipeline properties:");
         for (Object key : properties.keySet()) System.out.println(key + " = " + properties.get(key));
 
-        properties.put("annotators", "tokenize, ssplit, pos, custom.lemma, depparse, ner");
+        properties.put("annotators", "tokenize, ssplit, pos, custom.lemma, depparse, tokensregex");
 
         properties.setProperty("tokenize.options", "untokenizable=noneDelete");
         properties.setProperty("ssplit.newlineIsSentenceBreak", "always");
@@ -32,6 +37,7 @@ public class BasicFrenchNERPipeline {
         properties.setProperty("ner.fine.regexner.mapping", "src/main/resources/concepts.rules");
         // Remove entity mention sub-annotator
         properties.setProperty("ner.buildEntityMentions", "false");
+        properties.setProperty("tokensregex.rules", "src/main/resources/testing.rules");
 
         System.out.println("\nModified pipeline properties:");
         for (Object key : properties.keySet()) System.out.println(key + " = " + properties.get(key));
@@ -44,6 +50,17 @@ public class BasicFrenchNERPipeline {
         Annotation document = new Annotation(text);
         pipeline.annotate(document);
 
-        pipeline.xmlPrint(document, Files.newOutputStream(Paths.get("src/main/resources/results.xml")));
+        Files.deleteIfExists(Paths.get("src/main/resources/results.xml"));
+        Files.write(Paths.get("src/main/resources/results.xml"),
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<annotations>\n".getBytes(),
+                StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                Files.write(Paths.get("src/main/resources/results.xml"),
+                        String.format("<token>\n<word>%s</word>\n<POS>%s</POS>\n<NER>%s</NER>\n</token>\n", token.word(), token.tag(), token.ner()).getBytes(),
+                        StandardOpenOption.APPEND);
+            }
+        }
+        Files.write(Paths.get("src/main/resources/results.xml"), "</annotations>".getBytes(), StandardOpenOption.APPEND);
     }
 }
