777f0f026d0c9906e0cf552dc72d606a89fdb00d
Rémy Zirnheld <remy.zirnheld@insee.fr>
2019-05-27 14:06:56 +0000

    modify lexic maker to analyze lemmas

--- test-core-nlp-fr/src/main/java/fr/insee/stamina/nlp/utils/LexicMaker.java
+++ test-core-nlp-fr/src/main/java/fr/insee/stamina/nlp/utils/LexicMaker.java
@@ -1,43 +1,46 @@
 package fr.insee.stamina.nlp.utils;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Locale;
-import java.util.TreeMap;
-import java.util.regex.Pattern;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import fr.insee.stamina.nlp.Product;
+import fr.insee.stamina.nlp.ProductExporter;
+import fr.insee.stamina.nlp.S3FileManager;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class LexicMaker {
-    private static TreeMap<String, Integer> lexique;
-    private static Pattern spliter = Pattern.compile("[ '\\u0019\n\\.;,\\(\\)\t\\u00A0[0-9]+!\\+’]");
+    private static StanfordCoreNLP nlp;
+    private static S3FileManager s3;
 
     public static void main(String args[]) throws Exception {
-        String inputFile = "src/main/resources/ip-texte.txt";
-        String output = "src/main/resources/ip-lexique.txt";
-        lexique = new TreeMap<>();
-        for (String line : Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8)) {
-            buildLexic(line);
-        }
-        writeLexic(Paths.get(output));
+        // TODO construire le dico de lemme à partir du fichier pour sortir les mots n'ayant pas de lemme & peut-être plus
+
+        ProductExporter exporter = ProductExporter.getInstance();
+        Properties properties = new Properties();
+        properties.load(IOUtils.readerFromString("src/main/resouces/auth-properties.props"));
+        exporter.initialize(properties);
+
+        s3 = S3FileManager.getInstance();
+        properties = new Properties();
+        properties.load(IOUtils.readerFromString("StanfordCoreNLP-french.properties"));
+        properties.put("annotators","tokenize, ssplit, pos");
+        nlp = new StanfordCoreNLP(properties);
+
+        List<Product> products = exporter.getProducts(51);
+        products.stream().filter(predicate).forEach(analyze);
     }
 
-    private static void buildLexic(String text) {
-        String[] words = spliter.split(text.toLowerCase(Locale.FRENCH));
-        for (String word : words) {
-            if (Pattern.matches("[A-Za-z\\-À-ÿ]+", word)) {
-                lexique.put(word, lexique.getOrDefault(word, 0) + 1);
-            }
-        }
-    }
+    private static Predicate<Product> predicate = product -> product.getDiffusionDate().before(Date.valueOf("2018-12-31"));
 
-    private static void writeLexic(Path target) throws IOException {
-        Files.deleteIfExists(target);
-        Files.createFile(target);
-        for (String key : lexique.keySet()) {
-            Files.write(target, String.format("%s\t%d\n", key, lexique.get(key)).getBytes(), StandardOpenOption.APPEND);
-        }
+    private static Consumer<Product> analyze = product -> {
+        // TODO : appeler du xslt qui appelle la methode du dessous !
+    };
+
+    public static void lemmatize(String text) {
+
     }
 }
