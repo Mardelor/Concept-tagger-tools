2881837fc5fab31b70cb348624f5ebb151927aa2
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-06-17 10:14:50 +0200

    update ci

--- french-lemmatiser/src/main/java/fr/insee/stamina/nlp/FrenchLemmaAnnotator.java
+++ french-lemmatiser/src/main/java/fr/insee/stamina/nlp/FrenchLemmaAnnotator.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -18,14 +19,20 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * French lemmatizer.
+ * French lemmatizer. Map words to lemma
  * 
- * @author Franck
+ * @author Franck, rem
  */
 public class FrenchLemmaAnnotator implements Annotator {
 
+	/**
+	 * map words to lemma
+	 */
 	private Map<String, String> wordToLemma = new HashMap<>();
 
+	/**
+	 *
+	 */
 	public FrenchLemmaAnnotator(String name, Properties properties) throws IOException {
 		InputStream input;
 		if (properties.containsKey("french.lemma.lemmaFile")) {
@@ -35,7 +42,7 @@ public class FrenchLemmaAnnotator implements Annotator {
 		}
 		Function<String, String[]> split = line -> line.split("\\t");
 		Consumer<String[]> fillmap = items -> wordToLemma.put(items[0], items[1]);
-		new BufferedReader(new InputStreamReader(input))
+		new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))
 				.lines()
 				.map(split)
 				.forEach(fillmap);
